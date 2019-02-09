ff74df073518e3c61305718f1a2c0f194fc9e899
daviidbartlett <daviidbartlett@gmail.com>
2019-02-09 11:58:27 +0000

    make fetchPokemonNames function

--- src/components/BottomConsole.jsx
+++ src/components/BottomConsole.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import EvolutionChain from "./EvolutionChain";
+import PokeList from "./PokeList";
 
 const BottomConsole = () => {
   return (
     <div id="bottomConsole">
       <EvolutionChain />
+      <PokeList />
     </div>
   );
 };
--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
+import * as api from "api.js";
 
 export default class PokeList extends Component {
   state = {
     pokemon: []
   };
   render() {
-    return <div />;
+    const { pokemon } = this.state;
+    console.log(pokemon);
+    return (
+      <div id="pokeList">
+        <ul>
+          {pokemon.map((pkmn) => (
+            <li>{pkmn}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
+
+  fetchPokemonNames = () => {
+    api.getPokemonNames().then((pokeArr) => {
+      this.setState({ pokemon: pokeArr });
+    });
+  };
 }
