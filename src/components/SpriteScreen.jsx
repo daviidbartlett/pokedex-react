452f25f94779dec7f6afb32aa4856e6a3bf82f33
daviidbartlett <daviidbartlett@gmail.com>
2019-02-09 16:57:33 +0000

    style sprite frame

--- src/components/SpriteScreen.jsx
+++ src/components/SpriteScreen.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const SpriteScreen = ({ sprite, name }) => {
   return (
     <div id="spriteScreen">
-      <img className="sprite" src={sprite} alt={name} />
+      <div className="spriteFrame">
+        <img className="sprite" src={sprite} alt={name} />
+      </div>
+      <div className="triangle" />
     </div>
   );
 };
